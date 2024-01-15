7ad1ba74da76807c115f44916c233f972d3c8400
gunishmatta <gunishmatta@gmail.com>
2024-01-15 15:02:08 +0530

    fix formatting

--- control-plane/pkg/reconciler/broker/broker.go
+++ control-plane/pkg/reconciler/broker/broker.go
@@ -19,7 +19,6 @@ package broker
 import (
 	"context"
 	"fmt"
-	"knative.dev/eventing/pkg/auth"
 	"knative.dev/pkg/logging"
 	"strings"
 	"time"
@@ -34,6 +33,7 @@ import (
 	"k8s.io/utils/pointer"
 	eventing "knative.dev/eventing/pkg/apis/eventing/v1"
 	"knative.dev/eventing/pkg/apis/feature"
+	"knative.dev/eventing/pkg/auth"
 	duckv1 "knative.dev/pkg/apis/duck/v1"
 	"knative.dev/pkg/controller"
 	"knative.dev/pkg/network"
